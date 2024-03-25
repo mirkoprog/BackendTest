2230ec7c6780e7a4b98a8c2375a9d20de837eac3
admin <mirko@MIRKO>
2024-03-25 22:49:44 +0100

    first commit

---
+++ BackendTest.Manager/UtilityManager.cs
@@ -0,0 +1,61 @@
+﻿namespace BackendTest.Manager
+{
+    public class UtilityManager
+    {
+        public string CategorizeTriangle(int sideA, int sideB, int sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                return "INVALID PARAMS";
+
+            List<int> sides = new List<int> { sideA, sideB, sideC };
+            sides.Sort();
+
+            if (sides[0] + sides[1] <= sides[2])
+                return "Not a triangle";
+
+            if (sideA == sideB && sideB == sideC)
+                return "Equilateral triangle";
+
+            if (sideA == sideB || sideB == sideC || sideA == sideC)
+                return "Isosceles triangle";
+            
+            if (sideA * sideA + sideB * sideB == sideC * sideC ||
+                sideA * sideA + sideC * sideC == sideB * sideB ||
+                sideB * sideB + sideC * sideC == sideA * sideA)
+                return "Right triangle";
+
+            return "Scalene triangle";
+        }
+
+        public string ReverseWords(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                words[i] = new string(charArray);
+            }
+
+            return string.Join(" ", words);
+        }
+
+        public int GetNthFromEnd(int[] values, int n)
+        {
+            int index = values.Length - n;
+            int result = 0;
+
+            if (index >= 0 && index < values.Length)
+            {
+                result = values[index];
+                Console.WriteLine($"Il terzo elemento dalla fine è: {result}");
+            }
+            else
+            {
+                Console.WriteLine("L'indice specificato non è valido.");
+            }
+            return result;
+        }
+    }
+}
---
+++ BackendTest/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using BackendTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using BackendTest.Manager;
+
+namespace BackendTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UtilityManager _utilityManager;
+
+        public HomeController(UtilityManager utilityManager)
+        {
+            _utilityManager = utilityManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+       
+        public IActionResult GetNthFromEnd(string arr, int n)
+        {
+            // Converti la stringa di numeri separati da virgola in un array di interi
+            int[] numbers = arr.Split(',').Select(int.Parse).ToArray();
+
+            // Esegui il test
+            var result = _utilityManager.GetNthFromEnd(numbers, n);
+            ViewData["ResultGetNthFromEnd"] = result;
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ReverseWords(string regularPhrase)
+        {
+            var result = _utilityManager.ReverseWords(regularPhrase);
+            ViewData["ResultReverseWords"] = result;
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult TriangleCategorisation(int sideA, int sideB, int sideC)
+        {
+            var result = _utilityManager.CategorizeTriangle(sideA, sideB, sideC);
+            ViewData["ResultTriangleCategorisation"] = result;
+            return View("Index");
+        }
+    }
+}
